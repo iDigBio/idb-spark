f5109ecc698fe7f491f140ed6c5fd8461178f51d
Matthew J. Collins <mcollins@acis.ufl.edu>
2015-08-06 19:31:07 -0400

    Refactored output to be in per-recordset directories. Changed job format to cache pre-parsed CSVs now that have allocated enough memory properly for the cache to e in RAM. Saves ~30% run time.

--- unique-csvline.py
+++ unique-csvline.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import os
 import sys
 import unicodecsv
 from pyspark import SparkContext
@@ -13,6 +14,10 @@ def get_headers(fn):
 
 if __name__ == "__main__":
 
+    #recordset = "idigbio"
+    #recordset = "00d9fcc1-c8e2-4ef6-be64-9994ca6a32c3"
+    recordset = "0b17c21a-f7e2-4967-bdf8-60cf9b06c721"
+
     fields = ["dwc:scientificName",
               "dwc:specificEpithet",
               "dwc:collectionCode",
@@ -23,30 +28,32 @@ if __name__ == "__main__":
               "dwc:catalogNumber",
               "dwc:eventDate",
               "dwc:recordedBy"]
-
-
     #fields = ["dwc:occurrenceID"]
 
-    #fn = "data/idigbio/occurrence.csv"
-    fn = "data/0b17c21a-f7e2-4967-bdf8-60cf9b06c721/occurrence.txt"
+
+    out_dir = "out_{0}".format(recordset)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    fn = "data/{0}/occurrence.csv".format(recordset)
     headers = get_headers(fn)
 
-    sc = SparkContext(appName="TestUniqueCSVline")
+    sc = SparkContext(appName="UniqueCSVline")
     csvline = Csvline()
 
     records = sc.textFile(fn)
-    #parsed = records.map(lambda x: csvline.parse(x.encode("utf8"), headers) )
-    #parsed.cache()
+    parsed = records.map(lambda x: csvline.parse(x.encode("utf8"), headers) )
+    parsed.cache()
 
     for field in fields:
-        counts = records.map(lambda x: (csvline.parse(x.encode("utf8"), headers)[field], 1))
-        #counts = parsed.map(lambda x: (x[field], 1))
-        #sorted = counts.sortByKey()
+        #counts = records.map(lambda x: (csvline.parse(x.encode("utf8"), headers)[field], 1))
+        counts = parsed.map(lambda x: (x[field], 1))
+        ####sorted = counts.sortByKey()
         totals = counts.reduceByKey(add)
-        #totals.saveAsTextFile("out_{0}".format(field.replace(":", "_")))
+        #####totals.saveAsTextFile("out_{0}".format(field.replace(":", "_")))
 
         output = totals.collect()
-        out_fn = "out/unique_{0}.csv".format(field.replace(":", "_"))
+        out_fn = "{0}/unique_{1}.csv".format(out_dir, field.replace(":", "_"))
         with open(out_fn, "wb") as f:
             csvwriter = unicodecsv.writer(f, "excel")
             for (word, count) in output:
