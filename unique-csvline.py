e967014eb77fc1118a4ac6108fa81c80e1901160
Matthew J. Collins <mcollins@acis.ufl.edu>
2015-08-11 14:22:39 -0400

    Added options for processing the raw files, fixed file name pattern so using a URL in the header of the occurrence file is ok

--- unique-csvline.py
+++ unique-csvline.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os
 import sys
+import re
 import unicodecsv
 from pyspark import SparkContext
 from operator import add
@@ -14,11 +15,17 @@ def get_headers(fn):
 
 if __name__ == "__main__":
 
-    #recordset = "00d9fcc1-c8e2-4ef6-be64-9994ca6a32c3"
+    # small
+    recordset = "00d9fcc1-c8e2-4ef6-be64-9994ca6a32c3"
+    # medium
     #recordset = "0b17c21a-f7e2-4967-bdf8-60cf9b06c721"
-    recordset = "idigbio"
+    # all
+    #recordset = "idigbio"
 
-    fn = "data/{0}/occurrence.csv".format(recordset)
+    # set to "_raw" to process raw or "" to not
+    raw = "_raw"
+
+    fn = "data/{0}/occurrence{1}.csv".format(recordset, raw)
     headers = get_headers(fn)
 
     fields = ["dwc:scientificName",
@@ -36,7 +43,7 @@ if __name__ == "__main__":
     fields = headers
 
 
-    out_dir = "out_{0}".format(recordset)
+    out_dir = "out{0}_{1}".format(raw, recordset)
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
@@ -50,9 +57,12 @@ if __name__ == "__main__":
     parsed = records.map(lambda x: csvline.parse(x.encode("utf8"), headers) )
     parsed.cache()
 
+    # most fields have ":", some are URLs too in the raw data, make them usable
+    # as a file name. 
+    p = re.compile('[\W_]+')
     for field in fields:
 
-        out_fn = "{0}/unique_{1}.csv".format(out_dir, field.replace(":", "_"))
+        out_fn = "{0}/unique_{1}.csv".format(out_dir, p.sub("_", field))
         if os.path.exists(out_fn):
             continue
 
